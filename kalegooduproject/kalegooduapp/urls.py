f332e8d45c2df3d74fe9aa227d0ebcc3d12164ff
Pruthviraj-2004 <rajsavanur2003@gmail.com>
2024-08-09 17:32:44 +0530

    added new view classes and urls

--- kalegooduproject/kalegooduapp/urls.py
+++ kalegooduproject/kalegooduapp/urls.py
@@ -10,12 +10,14 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('home/', views.home, name='home'),
+    
     path('categories/', CategoryView.as_view(), name='category-list'),
     path('sale_types/', SaleTypeView.as_view(), name='sale-type-list'),
     path('products/', ProductView.as_view(), name='product-list'),
     path('product_images/', ProductImageView.as_view(), name='product-image-list'),
     path('category_images/', CategoryImageView.as_view(), name='category-image-list'),
     path('comments/', CommentView.as_view(), name='comment-list'),
+    
     path('categories/<int:category_id>/', CategoryDetailAPIView.as_view(), name='category-detail'),
     path('sale_types/<int:sale_type_id>/', SaleTypeDetailAPIView.as_view(), name='sale-type-detail'),
     path('products/<int:product_id>/', ProductDetailAPIView.as_view(), name='product-detail'),
@@ -23,10 +25,32 @@ urlpatterns = [
     path('category_images/<int:category_image_id>/', CategoryImageDetailAPIView.as_view(), name='category-image-detail'),
     path('comments/<int:comment_id>/', CommentDetailAPIView.as_view(), name='comment-detail'),
     path('banner_images/', BannerImageView.as_view(), name='banner_image_create'),
+    
     path('products_by_category/<int:category_id>/', ProductsByCategoryView.as_view(), name='products-by-category'),
-    path('categories-by-product/<int:product_id>/', CategoriesByProduct.as_view(), name='categories_by_product'),
+    path('categories_by_product/<int:product_id>/', CategoriesByProduct.as_view(), name='categories_by_product'),
 
     path('add_category/', CategoryCreateView.as_view(), name='category_create'),#add with images
     path('add_product/', ProductCreateView.as_view(), name='product_create'),#add with images
+    path('add_category_image/<int:category_id>/', AddCategoryImageView.as_view(), name='add_category_image'),
+    path('add_product_image/<int:product_id>/', AddProductImageView.as_view(), name='add_product_image'),
+
+    path('update_category/<int:category_id>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('update_category_image/<int:image_id>/', CategoryImageUpdateView.as_view(), name='category_image_update'),
+    path('update_full_category/<int:category_id>/', FullCategoryUpdateView.as_view(), name='category_update'),
+    path('update_product/<int:product_id>/', ProductUpdateView.as_view(), name='product_update'),
+    path('update_product_image/<int:image_id>/', ProductImageUpdateView.as_view(), name='product_image_update'),
+    path('update_full_product/<int:product_id>/', ProductFullUpdateView.as_view(), name='product_full_update'),
+    path('update_sale_type/<int:sale_type_id>/', SaleTypeUpdateView.as_view(), name='sale_type_update'),
+    path('update_comment/<int:comment_id>/', CommentUpdateView.as_view(), name='comment_update'),
+    path('update_banner_image/<int:banner_image_id>/', BannerImageUpdateView.as_view(), name='banner_image_update'),
+
+    path('sale_type/<int:sale_type_id>/delete/', SaleTypeDeleteView.as_view(), name='sale-type-delete'),
+    path('category/<int:category_id>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
+    path('product/<int:product_id>/delete/', ProductDeleteView.as_view(), name='product-delete'),
+    path('category_image/<int:category_image_id>/delete/', CategoryImageDeleteView.as_view(), name='category-image-delete'),
+    path('product_image/<int:product_image_id>/delete/', ProductImageDeleteView.as_view(), name='product-image-delete'),
+    path('comment/<int:comment_id>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('banner_image/<int:banner_image_id>/delete/', BannerImageDeleteView.as_view(), name='banner-image-delete'),
+
 
 ]
--- kalegooduproject/kalegooduapp/views.py
+++ kalegooduproject/kalegooduapp/views.py
@@ -337,3 +337,409 @@ class CategoriesByProduct(APIView):
         except Product.DoesNotExist:
             return Response({'error': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
     
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CategoryUpdateView(APIView):
+    def put(self, request, category_id):
+        try:
+            category = Category.objects.get(category_id=category_id)
+        except Category.DoesNotExist:
+            return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        category_data = {
+            'name': request.data.get('name', category.name),
+            'description': request.data.get('description', category.description)
+        }
+        category_serializer = CategorySerializer(category, data=category_data, partial=True)
+
+        if category_serializer.is_valid():
+            category_serializer.save()
+            return Response({'category': category_serializer.data}, status=status.HTTP_200_OK)
+        
+        return Response(category_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@method_decorator(csrf_exempt, name='dispatch')
+class CategoryImageUpdateView(APIView):
+    def put(self, request, image_id):
+        try:
+            category_image = CategoryImage.objects.get(category_image_id=image_id)
+        except CategoryImage.DoesNotExist:
+            return Response({'error': 'Category image not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        category_image_data = {
+            'category': request.data.get('category', category_image.category.category_id),
+            'alt_text': request.data.get('alt_text', category_image.alt_text)
+        }
+
+        if 'image' in request.FILES:
+            category_image_data['image'] = request.FILES['image']
+        
+        category_image_serializer = CategoryImageSerializer(category_image, data=category_image_data, partial=True)
+
+        if category_image_serializer.is_valid():
+            category_image_serializer.save()
+            return Response({'category_image': category_image_serializer.data}, status=status.HTTP_200_OK)
+        
+        return Response(category_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@method_decorator(csrf_exempt, name='dispatch')
+class FullCategoryUpdateView(APIView):
+    @transaction.atomic
+    def put(self, request, category_id):
+        try:
+            category = Category.objects.get(category_id=category_id)
+        except Category.DoesNotExist:
+            return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        category_data = {
+            'name': request.data.get('name', category.name),
+            'description': request.data.get('description', category.description)
+        }
+        category_serializer = CategorySerializer(category, data=category_data, partial=True)
+
+        if not category_serializer.is_valid():
+            return Response(category_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        category_serializer.save()
+
+        # Update existing images
+        image_updates = request.data.get('images', '[]')
+        for image_update in image_updates:
+            image_id = image_update.get('id')
+            if image_id:
+                try:
+                    category_image = CategoryImage.objects.get(category_image_id=image_id, category=category)
+                except CategoryImage.DoesNotExist:
+                    continue  # Skip images not found
+
+                category_image_data = {
+                    'alt_text': image_update.get('alt_text', category_image.alt_text),
+                }
+
+                if 'image' in image_update:
+                    category_image_data['image'] = image_update['image']
+
+                category_image_serializer = CategoryImageSerializer(category_image, data=category_image_data, partial=True)
+                if category_image_serializer.is_valid():
+                    category_image_serializer.save()
+                else:
+                    transaction.set_rollback(True)
+                    return Response(category_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Add new images
+        new_images = request.FILES.getlist('new_images')
+        for image in new_images:
+            category_image_data = {
+                'category': category.category_id,
+                'image': image,
+                'alt_text': request.data.get('alt_text', '')
+            }
+            category_image_serializer = CategoryImageSerializer(data=category_image_data)
+            if category_image_serializer.is_valid():
+                category_image_serializer.save()
+            else:
+                transaction.set_rollback(True)
+                return Response(category_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'category': category_serializer.data}, status=status.HTTP_200_OK)
+       
+@method_decorator(csrf_exempt, name='dispatch')
+class ProductUpdateView(APIView):
+    def put(self, request, product_id):
+        try:
+            product = Product.objects.get(product_id=product_id)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        product_data = {
+            'name': request.data.get('name', product.name),
+            'price': request.data.get('price', product.price),
+            'discounted_price': request.data.get('discounted_price', product.discounted_price),
+            'short_description': request.data.get('short_description', product.short_description),
+        }
+        product_serializer = ProductSerializer(product, data=product_data, partial=True)
+
+        if product_serializer.is_valid():
+            product_serializer.save()
+            return Response({'product': product_serializer.data}, status=status.HTTP_200_OK)
+
+        return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@method_decorator(csrf_exempt, name='dispatch')
+class ProductImageUpdateView(APIView):
+    def put(self, request, image_id):
+        try:
+            product_image = ProductImage.objects.get(product_image_id=image_id)
+        except ProductImage.DoesNotExist:
+            return Response({'error': 'Product image not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        product_image_data = {
+            'product': request.data.get('product', product_image.product.product_id),
+            'alt_text': request.data.get('alt_text', product_image.alt_text)
+        }
+
+        if 'image' in request.FILES:
+            product_image_data['image'] = request.FILES['image']
+
+        product_image_serializer = ProductImageSerializer(product_image, data=product_image_data, partial=True)
+
+        if product_image_serializer.is_valid():
+            product_image_serializer.save()
+            return Response({'product_image': product_image_serializer.data}, status=status.HTTP_200_OK)
+
+        return Response(product_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ProductFullUpdateView(APIView):
+    @transaction.atomic
+    def put(self, request, product_id):
+        try:
+            product = Product.objects.get(product_id=product_id)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        # Update Product Information
+        product_data = {
+            'name': request.data.get('name', product.name),
+            'price': request.data.get('price', product.price),
+            'discounted_price': request.data.get('discounted_price', product.discounted_price),
+            'short_description': request.data.get('short_description', product.short_description),
+        }
+        product_serializer = ProductSerializer(product, data=product_data, partial=True)
+
+        if not product_serializer.is_valid():
+            return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        product_serializer.save()
+
+        # Update Product Images
+        image_updates = request.data.get('images', '[]')
+        
+        # Process existing images
+        for image_update in image_updates:
+            image_id = image_update.get('id')
+            if image_id:
+                try:
+                    product_image = ProductImage.objects.get(product_image_id=image_id, product=product)
+                except ProductImage.DoesNotExist:
+                    continue  # Skip images not found
+
+                product_image_data = {
+                    'alt_text': image_update.get('alt_text', product_image.alt_text),
+                }
+
+                if 'image' in image_update:
+                    product_image_data['image'] = image_update['image']
+                
+                product_image_serializer = ProductImageSerializer(product_image, data=product_image_data, partial=True)
+                if product_image_serializer.is_valid():
+                    product_image_serializer.save()
+                else:
+                    transaction.set_rollback(True)
+                    return Response(product_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Process new images
+        new_images = request.FILES.getlist('new_images')
+        for image in new_images:
+            product_image_data = {
+                'product': product.product_id,
+                'image': image,
+                'alt_text': request.data.get('alt_text', '')
+            }
+            product_image_serializer = ProductImageSerializer(data=product_image_data)
+            if product_image_serializer.is_valid():
+                product_image_serializer.save()
+            else:
+                transaction.set_rollback(True)
+                return Response(product_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'product': product_serializer.data}, status=status.HTTP_200_OK)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class SaleTypeUpdateView(APIView):
+    def put(self, request, sale_type_id):
+        try:
+            sale_type = SaleType.objects.get(sale_type_id=sale_type_id)
+        except SaleType.DoesNotExist:
+            return Response({'error': 'SaleType not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        sale_type_data = {
+            'name': request.data.get('name', sale_type.name)
+        }
+        sale_type_serializer = SaleTypeSerializer(sale_type, data=sale_type_data, partial=True)
+
+        if sale_type_serializer.is_valid():
+            sale_type_serializer.save()
+            return Response({'sale_type': sale_type_serializer.data}, status=status.HTTP_200_OK)
+
+        return Response(sale_type_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CommentUpdateView(APIView):
+    def put(self, request, comment_id):
+        try:
+            comment = Comment.objects.get(comment_id=comment_id)
+        except Comment.DoesNotExist:
+            return Response({'error': 'Comment not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        comment_data = {
+            'product': request.data.get('product', comment.product.product_id),
+            'user_name': request.data.get('user_name', comment.user_name),
+            'rating': request.data.get('rating', comment.rating),
+            'text': request.data.get('text', comment.text)
+        }
+        comment_serializer = CommentSerializer(comment, data=comment_data, partial=True)
+
+        if comment_serializer.is_valid():
+            comment_serializer.save()
+            return Response({'comment': comment_serializer.data}, status=status.HTTP_200_OK)
+
+        return Response(comment_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class BannerImageUpdateView(APIView):
+    def put(self, request, banner_image_id):
+        try:
+            banner_image = BannerImage.objects.get(banner_image_id=banner_image_id)
+        except BannerImage.DoesNotExist:
+            return Response({'error': 'BannerImage not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        banner_image_data = {
+            'title': request.data.get('title', banner_image.title),
+            'image': request.FILES.get('image', banner_image.image)
+        }
+        serializer = BannerImageSerializer(banner_image, data=banner_image_data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'banner_image': serializer.data}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddCategoryImageView(APIView):
+    def post(self, request, category_id):
+        try:
+            category = Category.objects.get(pk=category_id)
+        except Category.DoesNotExist:
+            return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        image = request.FILES.get('image')
+        alt_text = request.data.get('alt_text', '')
+
+        if not image:
+            return Response({'error': 'No image file provided.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        category_image_data = {
+            'category': category_id,
+            'image': image,
+            'alt_text': alt_text
+        }
+        category_image_serializer = CategoryImageSerializer(data=category_image_data)
+
+        if category_image_serializer.is_valid():
+            category_image_serializer.save()
+            return Response({'category_image': category_image_serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(category_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+@method_decorator(csrf_exempt, name='dispatch')
+class AddProductImageView(APIView):
+    def post(self, request, product_id):
+        try:
+            product = Product.objects.get(pk=product_id)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        image = request.FILES.get('image')
+        alt_text = request.data.get('alt_text', '')
+
+        if not image:
+            return Response({'error': 'No image file provided.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        product_image_data = {
+            'product': product_id,
+            'image': image,
+            'alt_text': alt_text
+        }
+        product_image_serializer = ProductImageSerializer(data=product_image_data)
+
+        if product_image_serializer.is_valid():
+            product_image_serializer.save()
+            return Response({'product_image': product_image_serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(product_image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class SaleTypeDeleteView(APIView):
+    def delete(self, request, sale_type_id):
+        try:
+            sale_type = SaleType.objects.get(pk=sale_type_id)
+            sale_type.delete()
+            return Response({'message': 'SaleType deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        except SaleType.DoesNotExist:
+            return Response({'error': 'SaleType not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CategoryDeleteView(APIView):
+    def delete(self, request, category_id):
+        try:
+            category = Category.objects.get(pk=category_id)
+            category.delete()
+            return Response({'message': 'Category deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        except Category.DoesNotExist:
+            return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ProductDeleteView(APIView):
+    def delete(self, request, product_id):
+        try:
+            product = Product.objects.get(pk=product_id)
+            product.delete()
+            return Response({'message': 'Product deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CategoryImageDeleteView(APIView):
+    def delete(self, request, category_image_id):
+        try:
+            category_image = CategoryImage.objects.get(pk=category_image_id)
+            category_image.delete()
+            return Response({'message': 'Category image deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        except CategoryImage.DoesNotExist:
+            return Response({'error': 'Category image not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ProductImageDeleteView(APIView):
+    def delete(self, request, product_image_id):
+        try:
+            product_image = ProductImage.objects.get(pk=product_image_id)
+            product_image.delete()
+            return Response({'message': 'Product image deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        except ProductImage.DoesNotExist:
+            return Response({'error': 'Product image not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CommentDeleteView(APIView):
+    def delete(self, request, comment_id):
+        try:
+            comment = Comment.objects.get(pk=comment_id)
+            comment.delete()
+            return Response({'message': 'Comment deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        except Comment.DoesNotExist:
+            return Response({'error': 'Comment not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class BannerImageDeleteView(APIView):
+    def delete(self, request, banner_image_id):
+        try:
+            banner_image = BannerImage.objects.get(pk=banner_image_id)
+            banner_image.delete()
+            return Response({'message': 'Banner image deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        except BannerImage.DoesNotExist:
+            return Response({'error': 'Banner image not found.'}, status=status.HTTP_404_NOT_FOUND)
+        
